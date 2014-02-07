0a779d3a2a936bc03181bdf9229af72090a18775
Jon Jacky <jon@u.washington.edu>
2014-02-06 21:03:33 -0800

    add ed0.py, editor core extracted from ed.py

---
+++ samples/ed0.py
@@ -0,0 +1,10 @@
+"""
+ed0.py - editor core
+
+In this API there are no optional or keyword arguments.
+There is no error checking, no error messages, and no progress messages.
+Numeric line addresses use Python numbering conventions:
+the first line is number 0 and the last line number is length-1.
+Ranges use Python slice conventions: the first element is at the
+start index, the last element is at one less than the end index.
+"""
