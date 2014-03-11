e0cfd7004fa6bc49350e7ca5b39b9c7bf98b2954
Jon Jacky <jon@u.washington.edu>
2014-03-10 21:00:17 -0700

    add test_ed.sh and .sh.log, command mode tests working ed.py and ed0.py: fix incorrect unsaved changes mark after B file regenerate affected .log files

---
+++ test/ed/test_ed.sh
@@ -0,0 +1,74 @@
+# test_ed.sh - Same operations as test_ed.py but commands not API calls here
+#
+python -c "from ed import *; ed()" <<'END'
+B 42
+B test_ed.txt
+B ed.py.txt
+n
+e
+1,6p
+7l
+l
+l
+l
+l
+p
+100p
+p
+.p
+$p
+e
+b test_ed.txt
+e
+n
+D foo.txt
+D ed.py.txt
+n
+i
+Line 1
+Line 2
+Line 3
+.
+p
+1,$p
+a
+Line A
+Line B
+Line C
+.
+p
+1,$p
+i
+Line a
+Line b
+Line c
+.
+p
+1,$p
+4l
+i
+Line i
+Line ii
+Line iii
+.
+p
+1,$p
+6a
+Line I
+Line II
+Line III
+.
+p
+1,$p
+w
+p
+13l
+p
+d
+p
+.,$d
+p
+1,$p
+END
+echo ""
+echo "Remove test_ed.txt before running this script again, to obtain the same result"
