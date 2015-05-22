b8ee48bcf81b4bd6edab1c4bd4064a2ec05df1b7
Jon Jacky <jon@u.washington.edu>
2015-05-21 22:53:14 -0700

    docs/other.md: add Lua

---
+++ scripts.twisted/writer_tasks.py
@@ -0,0 +1,22 @@
+"""
+writer_tasks.py - Create Piety Writer tasks used by embedded and embedded.twisted
+
+When the Piety scheduler is running, task t0 writes to file w0.txt on
+every timeout, and task t1 writes to w1.txt on every other timeout.
+
+The shell command: tail -f w0.txt shows the t0 task in action.
+"""
+
+import piety # for Piety Task class, schedule data structure etc.
+from writer import Writer # applications
+
+w0,w1 = Writer(fname='w0.txt'),Writer(fname='w1.txt')
+
+# use this for t1.guard
+def alternate():
+    'Return True on every other timeout event.'
+    return bool(piety.ievent[piety.timer]%2)
+
+# writer tasks assigned guard=(lambda:False) so they don't start running
+t0 = piety.Task(handler=w0.write,input=piety.timer,enabled=piety.true)
+t1 = piety.Task(handler=w1.write,input=piety.timer,enabled=alternate)
