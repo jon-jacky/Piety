7a0b0229feb4e6a2bbfd784d65014b1985ecf170
Jon Jacky <jon@u.washington.edu>
2015-07-28 08:49:23 -0700

    applications/printer.py: tweak style rename Timestamp makeline method to __call__ in Buffer, end_phase is boolean not integer, easier to invert

--- applications/printer.py
+++ applications/printer.py
@@ -14,9 +14,9 @@ import sys, datetime
 
 class Timestamp(object):
     """
-    Has a makeline method that returns a single line including
+    Has a __call__ method that returns a single line including
      a sequence number, label, and the current time.  
-    The sequence number starts at 0 and advances on each call to makeline
+    The sequence number starts at 0 and advances on each __call__
     The label is an __init__ arg
     """
 
@@ -25,10 +25,12 @@ class Timestamp(object):
         self.seqno = 0
         self.label = label
 
-    def makeline(self):
+    # This is the only method we ever use, just call it by the object's name
+    def __call__(self):
         """ 
-        returns a line with sequence number, label, and also timestamp:
+        Returns a line with sequence number, label, and also timestamp:
          5 main.txt 2013-07-13 11:32:42.231009
+        then increments the sequence number
         """
         # no trailing \n, print adds that by default
         s =  '%6d %s %s' % (self.seqno, self.label, datetime.datetime.now())
@@ -41,15 +43,14 @@ class Buffer(object):
     'stand-in for an ed0 text buffer'
     def __init__(self):
         self.lines = [] # buffer contents
-        # For processing writes to buffer via Python print(s, file=buffer)
-        # Experiments show that this Python print calls Buffer write *twice*,
-        # first write for the contents s, second write for end string
-        # even when end string is default \n or empty ''     
-        # So here we alternate reading contents and discarding end string
         self.contents = ''
-        self.end_phase = 0  # assumes first write is *not* the end string
+        self.end_phase = False # assumes first write is *not* the end string
 
-    # Invoked by print(s, file=buffer)
+    # Invoked by print(s, file=buffer), writes s to buffer
+    # Experiments show that this Python print calls Buffer write *twice*,
+    # first write for the contents s, second write for end string
+    # even when end string is default \n or empty ''     
+    # So here we alternate reading contents and discarding end string
     def write(self, s):
         #print([c for c in s]) # DEBUG reveals second write for end string
         #print('end_phase %s' % self.end_phase) # DEBUG
@@ -59,7 +60,7 @@ class Buffer(object):
         else:
             # store contents string until we get end string
             self.contents = s
-        self.end_phase = 0 if self.end_phase else 1 # 0 1 0 1 ...
+        self.end_phase = not self.end_phase # False True False ...
 
 
 # Test
@@ -71,14 +72,14 @@ buf0, buf1 = Buffer(), Buffer()
 def main():
     print('Two printers interleaving, each printing five lines to stdout')
     for i in range(5):
-        print(ts0.makeline()) # no file=... print to stdout
-        print(ts1.makeline())
+        print(ts0()) # no file=... print to stdout
+        print(ts1())
     print("""Two printers interleaving, each printing five lines to different buffer
  then print each buffer in turn""")
     # first print to both buffers
     for i in range(5):
-        print(ts0.makeline(), file=buf0) # use file=... print to buffer
-        print(ts1.makeline(), file=buf1)
+        print(ts0(), file=buf0) # use file=... print to buffer
+        print(ts1(), file=buf1)
     # then print contents of each buffer
     for line in buf0.lines:
         print(line.rstrip()) # buffer contents already include final \n
