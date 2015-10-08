7bfe9df99a28125ff7626bcb6e4a195e088cab56
Jon Jacky <jon@u.washington.edu>
2015-10-08 08:55:42 -0700

    editors/edd.py: remove frame, just one window clean up style and comments, this simple version has just a single window

--- editors/edd.py
+++ editors/edd.py
@@ -13,31 +13,31 @@ ed.destination = null
 
 prompt = '' # command prompt
 
-# Position display elements.  For now, just two regions from top to bottom:
-#  1. Frame, holds all text buffer window including buffer status line(s)
+# The frame is the entire region on the display occupied by the editor,
+#  for example, a terminal window on the host desktop.
+# Position display elements in frame. For now, just two regions, top to bottom:
+#  1. single window into text buffer including buffer status line(s)
 #  2. scrolling command input region
 # Line numbers on display and in each element are 1-based as in ed and ansi.
 
 nlines, ncols = terminal.dimensions()
 
-# Defaults, might be updated while program is running, especially cmd_h:
+# Default dimensions, might be updated while edd is running, especially cmd_h:
 
-win_1 = 1 # line number on display of first line of first window
+win = None # single window instance will go here
+
+win_1 = 1 # line number on display of first line of window
+win_h = None # total number of lines in window
 status_h = 1 # height (lines) of buffer status, now just a status line
-             # for now this must be the same for all windows
 cmd_h = 2  # height (lines) of scrolling command region at the bottom
 cmd_1 = None # line num on display of 1st line of scrolling command region
 cmd_n = None #  " bottom "
-win_h = None # total number of lines in frame, all windows (vertical stack)
-
-# Frame
-frame = list() # list of windows
 
 # Values saved before ed command so we can test for changes after
 (cmd_h0, current0, filename0) = (None, None, None)
 
 def save_parameters():
-    'Save window parameters before ed cmd so we can test for changes after'
+    'Save frame + window info before ed cmd so we can test for changes after'
     global cmd_h0, current0, filename0
     cmd_h0, current0, filename0 = cmd_h, ed.current, ed.buf.filename
 
@@ -47,7 +47,7 @@ def print_saved():
 
 def frame_changed():
     'Frame dimensions or locations changed'
-    return cmd_h != cmd_h0 # only one window for now
+    return cmd_h != cmd_h0
 
 def file_changed():
     'Current buffer changed or different file loaded in current buffer'
@@ -58,27 +58,32 @@ def text_cmd():
     return ed.cmd_name in 'aicds' # append, insert, change, delete, substitute
 
 def calc_frame():
-    'Calculate dimensions and location of window'
-    # Currently there is only one window
-    # scrolling command region at the bottom
+    'Calculate dimensions and location of window and scrolling command region'
     global cmd_1, cmd_n, win_h
-    cmd_1 = nlines - cmd_h + 1 # first line after cmd_h lines
-    cmd_n = nlines # bottom of display
-    # text window element - just one window for now, may add others later
-    win_h = nlines - cmd_h # fills remaining space
+    cmd_1 = nlines - cmd_h + 1 # scrolling cmd region, index of first line
+    cmd_n = nlines # last line on display
+    win_h = nlines - cmd_h # text window including status fills remaining space
+    # win_1, ncols unchanged
 
 def set_command_cursor():
     'Put cursor at input line in scrolling command region'
-    display.put_cursor(cmd_n, 1) # bottom line
+    display.put_cursor(cmd_n, 1) # last line on display
 
 def display_frame():
     # called from init_session and update_frame
     display.put_cursor(1,1) # origin, upper left corner
     display.erase_display() 
-    win = frame[0] # FIXME just one window for now
-    win.update_window(ed.command_mode) # just one window
+    win.update_window(ed.command_mode)
     display.set_scroll(cmd_1, cmd_n) 
     set_command_cursor()
+
+def update_frame():
+    """
+    Call when frame dimensions may have changed.
+    """
+    calc_frame() # recalculate global cmd_1 cmd_n win_h
+    win.resize(win_1, win_h, ncols)
+    display_frame()
 
 def init_session(*filename, **options):
     """
@@ -92,22 +97,13 @@ def init_session(*filename, **options):
         prompt = options['p'] 
     if 'h' in options:
         cmd_h = options['h'] 
-    # must calc frame before we create first window
+    # must calc frame before we create window
     calc_frame() # initialize cmd_1, cmd_n etc.
-    win = window.Window(ed.buf, win_1, win_h, ncols) # create first window
-    frame.append(win) # add first window to frame, possibly more to come
-    display_frame()
-
-def update_frame():
-    'like init_session but no args and no window creation'
-    calc_frame() # recalculate cmd_1, cmd_n, cmd_h
-    win = frame[0] # FIXME just one window for now
-    win.resize(win_1, win_h, ncols)
+    win = window.Window(ed.buf, win_1, win_h, ncols)
     display_frame()
 
 def update_display():
     'Show window, cursor, status line. Set scroll to input region,place cursor'
-    win = frame[0]  # FIXME just one window for now
     win.locate_cursor() # assign new cursor_i
     # recalculate frame and redisplay everything
     if frame_changed():
@@ -146,7 +142,6 @@ def cmd(line):
         else:
             ed.cmd(line) # non-blocking
         if ed.cmd_name in 'bBeED':
-            win = frame[0] # FIXME just one window for now
             win.buf = ed.buf # ed.buf might have changed
         update_display()
     except BaseException as e:
