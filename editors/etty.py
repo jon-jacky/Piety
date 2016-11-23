dafcd9c6e28b49c1a8a20d050f2a1a2adfebfd5b
Jon Jacky <jon@u.washington.edu>
2016-11-22 21:29:44 -0800

    editors/etty.py: override some new console defaults, use new console run method

--- editors/etty.py
+++ editors/etty.py
@@ -1,24 +1,22 @@
 """
 etty.py - Run ed line editor session, but use console/command.py
  instead of Python input() to collect and edit command line and inserted text.
-Use keymaps to provide retro printing-terminal-style editing and history.
+ Use keymaps to provide retro printing-terminal-style editing and history.
 """
 
-import ed, console as con, key, keyboard
+import ed, console as con, terminal
 
-console = con.Console(prompt=':', do_command=ed.do_command,
+console = con.Console(prompt=':', reader=terminal.getchar,
+                      command=con.LineInput(),
+                      do_command=ed.do_command,
                       stopped=(lambda command: ed.quit),
-                      # accept default keymap for printing terminal
+                      keymap=con.printing_keymap,
                       mode=(lambda: ed.command_mode), # True or False
                       behavior={ False: ('', con.printing_insertmode_keymap) })
 
 def main():
     ed.quit = False # previous quit might have set it True
-    console.restart()
-    while (not console.stopped() and 
-           console.command.line not in console.job_commands):
-        console.handler() # q command sets ed.quit True, forces exit
-    console.restore()
+    console.run()
 
 if __name__ == '__main__':
     main()
