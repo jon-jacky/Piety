4f07614a826b4a790f566c393c3d558174650376
Jon Jacky <jon@u.washington.edu>
2018-03-13 22:04:54 -0700

    edo: add x_command, samysh: add run_script and add_command

--- editors/edo.py
+++ editors/edo.py
@@ -5,54 +5,29 @@ edo.py - ed + wyshka, ed with command interpreter that also provides python
 
 import ed, samysh, wyshka
 
-# defined here not in samysh because it depends on ed module and ed cmd fmt
-def run_script(paramstring, do_command):
-    """
-    Run script of commands from another buffer with optional echo and delay
-     paramstring is usually 'bufname echo delay'
-     bufname is not optional, and it cannot be the current buffer.
-     echo - optional, default True; delay - optional, default 0.2 sec
-    do_command is function to call on command string after applying echo, delay
-    """
-    params = paramstring.split()
-    if params:
-        bufname = ed.match_prefix(params[0], ed.buffers)
-        if bufname in ed.buffers and bufname != ed.current:
-            echo, delay = True, 0.2
-            if len(params) > 1:
-                echo = params[1] not in ('0','f','false','F','False')
-            if len(params) > 2:
-                try:
-                    delay = float(params[2])
-                except ValueError:
-                    pass # use default
-            # Must define samysh_do... here, echo, delay might differ each call
-            _do_command = samysh.show_command(do_command=do_command,
-                                              echo=echo, delay=delay)
-            for line in ed.buffers[bufname].lines[1:]: # lines[0] always empty
-                _do_command(line.rstrip()) # remove terminal \n 
-        else:
-            print('? buffer name')
-    else:
-        print('? buffername echo delay')
-
-def add_scripting(name, do_command):
-    'Augment do_command with a new command (name) to run script from buffer'
-    def _do_command(line):
-        line = line.lstrip()
-        if ed.command_mode and line.startswith(name):
-            run_script(line[1:], do_command) # assumes command name is 1 char
-        else:
-            do_command(line)
-    return _do_command
-
 # add embedded python interpreter
 _do_command = wyshka.shell(do_command=ed.do_command, 
                            command_mode=(lambda: ed.command_mode),
                            command_prompt=(lambda: ed.prompt))
 
-# add x command to run script with optional echo and delay
-do_command = add_scripting('x', _do_command)
+def x_command(line):
+    'run script from buffer with optional echo and delay'
+    line = line.lstrip()
+    command, rest = line[0], line[1:].lstrip()
+    if command == 'x':
+        bufname, _, params = rest.partition(' ')
+        bufname = ed.match_prefix(bufname, ed.buffers)
+        if bufname in ed.buffers and bufname != ed.current:
+            lines = ed.buffers[bufname].lines[1:] # lines[0] always empty
+            samysh.run_script(params, lines, _do_command)
+        else:
+            print('? buffer name')
+        return True
+    else:
+        return False
+
+# add command to run script from buffer with optional echo and delay
+do_command = samysh.add_command(x_command, _do_command)
 
 def startup(*filename, **options):
     ed.startup(*filename, **options)
--- shells/samysh.py
+++ shells/samysh.py
@@ -1,24 +1,21 @@
 """
 samysh.py - Execute command with optional echo and delay.
+            Run script, execute each command with optional echo and delay.
             Useful for observing the execution of test scripts.
 """
 
 import time # for blocking time.sleep(), FIXME write nonblocking piety.sleep()
 
-def samysh(do_command=(lambda line: None), echo=True, delay=None):
+def show_command(do_command=(lambda line: None), echo=True, delay=None):
     """
     Return a function with one argument (a string, the command line) that
-    executes a command with optional echo and delay.
-
+     executes a command with optional echo and delay.
     This function has three optional keyword arguments with defaults:
-
     do_command is a callable with one string argument - the command line -
-    that executes an application command.  Default does nothing.
-     
+     that executes an application command.  Default does nothing.
     echo - if True, print each line before executing it.  Default False.
-
     delay - seconds to wait after executing line (float, can be < 1.0).
-    Default no delay.
+     Default no delay.
     """
     def _do_command(line):
         if echo: 
@@ -28,12 +25,46 @@ def samysh(do_command=(lambda line: None), echo=True, delay=None):
             time.sleep(delay) # FIXME replace with non-blocking piety.sleep()
     return _do_command
 
+def run_script(paramstring, lines, do_command):
+    """
+    Run script of commands from another buffer with optional echo and delay
+     paramstring is usually 'bufname echo delay'
+     echo - optional, default True; delay - optional, default 0.2 sec
+     do_command is fcn to call on command string after applying echo, delay
+    """
+    params = paramstring.split()
+    echo, delay = True, 0.2
+    if len(params) > 0:
+        echo = params[0] not in ('0','f','false','F','False')
+    if len(params) > 1:
+        try:
+            delay = float(params[1])
+        except ValueError:
+            pass # use default
+    # Must define show_command here, echo and delay might differ on each call.
+    _do_command = show_command(do_command=do_command,
+                               echo=echo, delay=delay)
+    for line in lines:
+        _do_command(line.rstrip()) # remove terminal \n 
+
+
+def add_command(new_command, do_command):
+    """
+    Augment do_command with new_command.  
+    When new_command returns False, run do_command.
+    """
+    def _do_command(line):
+        attempted = new_command(line)
+        if not attempted:
+            do_command(line)
+    return _do_command
+
 # Test
 
 def execute(line):
     print('Executing %s ...' % line)
 
-do_command = samysh(do_command=execute, echo=True, delay=2.0)
+do_command = show_command(do_command=execute, echo=True, delay=2.0)
 
 def main():
     while True:
