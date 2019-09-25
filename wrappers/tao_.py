77b63ab53044ae80b213d1c1f624182cd3fb26d6
Jon Jacky <jon@u.washington.edu>
2019-09-25 13:12:44 -0700

    tao_.py: add explanation in header

--- wrappers/tao_.py
+++ wrappers/tao_.py
@@ -1,19 +1,34 @@
 """
-tao_.py, wrapper for tao.py from https://github.com/nyxyn/tao
+tao_.py, print a chapter from the Tao Te Ching.
 
+Wrapper for tao.py from https://github.com/nyxyn/tao
+
+To import this wrapper:
+
+  import tao_ as tao
+
+To print a random chapter:
+
+  tao.main()
+
+To print chapter 42 (etc.):
+
+  tao.main(42)
+
+This wrapper invokes the original tao.py script from github without any
+changes, so it prints a chapter on initial import, then rereads the same
+json file with the entire book text every time it is invoked.
+
+This wrapper is named tao_.py (with a trailing underscore) to avoid a
+name clash with the original tao.py.  We must import both.
 """
 
 import sys
 
-# Avoid name clashes.  In a Piety session, import this module by:
-# import tao_ as tao
-# Here we import the installed program, 
+# import the installed program,
 # must simulate command line args because code that runs on import uses them
 sys.argv = ['tao'] # no argv[1]
 import tao as _tao
-
-# In a Piety session, invoke this program with tao.main() to print random
-# chapter or tao.main(42) to print chapter 42.
 
 def main(*chapter):
     # simulate command line args so run can find them
@@ -21,7 +36,5 @@ def main(*chapter):
     if chapter:
         sys.argv.append(str(chapter[0]))
 
-    # FIXME - very inefficient, loads json for entire book each time
     _tao.run()
-
 
