92cf8f301804b598ac30c71d80a21ab4872663b6
Jon Jacky <jon@u.washington.edu>
2023-02-05 20:30:12 -0800

    sked, skedinit: add editing functions a d y and file w(rite)

--- editors/sked.py
+++ editors/sked.py
@@ -5,13 +5,9 @@ sked.py - Stone Knife Editor, minimal line editor inspired by classic Unix ed,
 No main program!  Editor commands are just functions defined here, to
 call from the Python REPL.
 
-Global data used by these functions, includng the text buffer, are
-defined and initialized elsewhere, in skedinit.py, so this sked module
-can be reloaded into a running editor session after revising or adding
-functions, without re-initializing variables and losing data.
-
-skedinit is *conditionally* executed, only the first time this module
-is imported in a Python session.  See code below.
+Global variables used by these functions are defined and initialized
+in skedinit.py, which must be executed before calling any of the
+functions here.
 
 The name sked is inspired by Kragen Sitaker's Stone Knife Forth.
 """
@@ -24,23 +20,48 @@ try:
 except:
     exec(open("skedinit.py").read())
 
+# utility functions
+
 def S():
     'Return index of last line in buffer.  S looks a bit like classic ed $'
     return len(buffer)-1  # -1 because of zero based index
 
+# File and buffer functions
+
 def e(fname): 
     'Load file named fname into buffer, replacing any previous contents.'
-    global filename, buffer, o
-    success = False # Might crash if fname doesn't exist etc.
-    with open(fname, mode='r') as fd:
-        # fd.readlines reads file into a list of strings, one per line
-        # First line of file is at index 1 not 0
-        buffer = ['\n'] + fd.readlines() # each string in buffer ends with \n
+    global filename, buffer, o, saved
+    try:
+        with open(fname, mode='r') as fd:
+            # fd.readlines reads file into a list of strings, one per line
+            # First line of file is at index 1 not 0
+            buffer = ['\n'] + fd.readlines() # each line in buffer ends with \n
+    except FileNotFoundError:
+        buffer = ['\n'] # start new file
+    filename = fname
+    o = S() # index of last line 
+    print('%s, %d lines' % (filename, o))
+    saved = True
+
+def w(fname=None):
+    'Write buffer to file, default name is in filename.'
+    global filename
+    fname = fname if fname else filename
+    success = False # Might fail if path doesn't exist, no permission etc.
+    with open(fname, 'w') as fd:
+        fd.writelines(buffer[1:]) # first line of file is at index 1 not 0
         success = True
     if success:
         filename = fname
-        o = S() # index of last line 
-        print('%s, %d lines' % (filename, o))
+        print('%s, %d lines' % (filename, S()))
+        saved = True
+
+# File viewing functions
+
+def st():
+    'status, print information about editing session'
+    print('%s, %d lines, at line %d, %s' %  (filename, S(), o,
+          'no changes need to be saved' if saved else 'unsaved changes'))
 
 def s(target=None):
     """
@@ -99,12 +120,6 @@ def printline(iline):
         print('? end of buffer')
         return False
 
-def advance():
-    'Advance dot to next line and print'
-    global o
-    iline = o + 1
-    printline(iline)
-
 def p(start=None, end=None):
     """
     Print range of lines from buffer, from index start through end.
@@ -142,3 +157,57 @@ def mv(nlines=None):
     start, end = o-(pagesize+1), o-1
     p(start, end)
     o = start # p puts dot at end
+
+# Editing functions
+
+def a(iline=None):
+    'a(ppend) lines after iline (default dot), type just . on a line to exit'
+    global buffer, o, saved
+    iline = iline if iline else o
+    if iline < 0 or iline > S():
+        print('? %d out of range, last line is %d' % (iline, S()))
+        return
+    o = iline
+    while True:
+        success = False
+        line = input()  # Can this fail?  Yes, by ^C for example
+        success = True
+        if success:
+            if line == '.':
+                return
+            buffer[o+1:o+1] = [line + '\n'] # sic, append line after dot
+            o +=1
+            saved = False
+
+def d(start=None, end=None):
+    """
+    d(elete) lines from buffer and save in yank (paste) buffer
+    set dot to line preceding deletion, can use a(ppend) to replace deletion
+    """
+    global buffer, yank, o, saved
+    start = start if start else o
+    end = end if end else start
+    if start < 1 or start > S():
+        print('? start %d out of range, last line is %d' % (start, S()))
+        return
+    if end != start and (end < 1 or end > S()):
+        print('? end %d out of range, last line is %d' % (start, S()))
+        return
+    if start > end:
+        print('? start %d follows end %d' % (start, end))
+        return
+    yank = buffer[start:end+1] # range includes end, unlike Python slices
+    buffer[start:end+1] = [] 
+    o = start-1
+    saved = False
+
+def y(iline=None):
+    'Append yank buffer contents after iline (defalt dot)'
+    global buffer, o, saved
+    iline = iline if iline else o
+    if iline < 0 or iline > S():
+        print('? %d out of range, last line is %d' % (iline, S()))
+        return
+    buffer[iline+1:iline+1] = yank # append yank buffer contents after iline
+    o = iline + len(yank)
+    saved = False
--- editors/skedinit.py
+++ editors/skedinit.py
@@ -1,15 +1,6 @@
 """
-skedinit - Define and initialize global variables used by editor
-           functions in the sked module.
-
-The variables defined and initialized here store the state of the sked
-editor session: the text in the buffer, the location of the current
-line 'dot', etc.  The statements here must be executed before calling
-any of the functions in sked.
-
-The sked editor functions are in a separate module so they can
-be reloaded into an editor session after revising or adding functions,
-without re-initializing these variables and losing data.
+skedinit - Define and initialize global variables used by editor functions
+           in the sked module: text buffer, current location, etc.
 """
 
 # buffer is zero indexed, but we want first line of file to be at index 1
@@ -17,6 +8,9 @@ without re-initializing these variables and losing data.
 buffer = ['\n']  # '\n' at index 0 is never used
 o = 0            # dot, index of current line in buffer.  o looks like ed .
 
-filename = 'main'     # default, reassigned by e command
-searchstring = 'main' # default, reassigned by s(earch) and r(everse) commands
-pagesize = 12         # default, reassigned by z command
+filename = 'test.txt' # reassigned by e(dit) and w(rite) commands
+searchstring = 'def ' # reassigned by s(earch) and r(everse) commands
+pagesize = 12         # reassigned by v and mv page up/down commands
+saved = True          # True when no unsaved changes, safe to run e(dit).
+
+yank = [] # yank (paste) buffer
