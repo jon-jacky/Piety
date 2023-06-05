ffef645f627ff6d8dc54c883ef1812b4229033a0
Jon Jacky <jon@u.washington.edu>
2023-06-05 13:44:59 -0700

    pmacs: add prompting, forward and backward search

--- editors/pmacs.py
+++ editors/pmacs.py
@@ -9,7 +9,7 @@ Then, if we edit more commands into pm, we can load them without restarting
 the session by reload(pmacs).  The argument to reload must be the module name.
 """
 
-import util, terminal, key, edsel
+import util, terminal, key, display, edsel
 import sked as ed
 from keyseq import keyseq
 
@@ -19,21 +19,64 @@ def append():
     edsel.a()
     terminal.set_char_mode()
 
+def request(prompt):
+    display.put_cursor(promptline, 1)
+    display.kill_whole_line()
+    terminal.set_line_mode()
+    response = input(prompt)
+    terminal.set_char_mode()
+    return response
+
+def request_search():
+    if not prev_k in (key.C_s, key.C_r):
+        response = request(f'search (default {ed.searchstring}): ')
+        if response: ed.searchstring = response
+
+def fwd_search():
+    request_search()
+    edsel.s()
+
+def bkwd_search():
+    request_search()
+    edsel.r()
+
 # Table from keys to editor functions
 keycode = {
+    # cursor movement
     key.C_n: edsel.l,  # next line
     key.C_p: edsel.rl,  # previous line
     key.C_v: edsel.v,   # page down
     key.M_v: edsel.rv,  # page up
     key.M_lt: (lambda: edsel.p(1)), # go to top, line 1.  lt is <.
     key.M_gt: (lambda: edsel.p(ed.S())), # go to bottom, last line.  gt is >.
+    # search
+    key.C_s: fwd_search,
+    key.C_r: bkwd_search,
+    # editing
     key.C_k: edsel.d,   # delete line
     key.C_y: edsel.y,   # yank (paste) deleted line
-    key.M_q: edsel.wrap,  # wrap, single long line
     key.cr: append,   # open line and enter append mode 
+    # formatting
+    key.M_q: edsel.wrap,  # wrap, single long line
+    # buffers and files
+    key.C_x + key.C_s : edsel.w,  # write file, with stored filename
+    # miscellaneous
     key.C_l: edsel.refresh, # refresh, frame
-    key.C_x + key.C_s : edsel.w  # write file, with stored filename
 }
+
+promptline = 21
+
+def open_promptline():
+    global promptline
+    promptline = edsel.flines+1 # may have changed since prev pm() call
+    display.set_scroll(promptline+1, edsel.tlines) # open prompt line
+    display.put_cursor(promptline, 1)
+    display.kill_whole_line() 
+
+def close_promptline():
+    display.set_scroll(promptline, edsel.tlines) # dismiss prompt line
+
+prev_k = key.C_z
 
 def pm():
     """
@@ -41,6 +84,8 @@ def pm():
     Supported keys and the fcns they invoke are expressed in keycode table.
     Exit by typing C_z (that's ^Z).
     """
+    global prev_k
+    open_promptline()
     terminal.set_char_mode()
     while True:
         c = terminal.getchar()
@@ -51,4 +96,7 @@ def pm():
             else:
                 fcn = keycode.get(k, lambda: util.putstr(key.bel))
                 fcn()
+                prev_k = k
     terminal.set_line_mode()
+    close_promptline()
+    display.put_cursor(edsel.tlines, 1) # return cursor to command line
