627abf554139a4cee03c50826979cb756d832d28
Jon Jacky <jon@u.washington.edu>
2023-08-18 22:21:24 -0700

    pmacs: editline commands working, but cursor goes to command line, doesnt stay in window yet

--- editors/pmacs.py
+++ editors/pmacs.py
@@ -1,11 +1,11 @@
 """
-pmacs.py - display editor that uses emacs control keys.
+pmacs.py - display editor that uses emacs control terminal, keys.
 
 pmacs might mean 'Python emacs' but actually means 'poor emacs'
 or 'pathetic emacs' or maybe 'Passing grade emacs, just above Fail'.
 """
 
-import key, edsel, dmacs, editline 
+import terminal, key, keyseq, display, edsel, dmacs, editline 
 import sked as ed
 
 # Some functions do not use keycode arg but pmcmd and pm require it to be there
@@ -17,7 +17,7 @@ def open_line(keycode):
     """
     suffix = ed.buffer[ed.dot:] # including final \n
     # Calls editline.kill_line, thanks to key.C_k, not keycode
-    editline.elcmd_aref(key.C_k, editline.point, ed.buffer[dot])
+    editline.elcmd_aref(key.C_k, editline.point, ed.buffer[ed.dot])
     ed.buffer[ed.dot+1:ed.dot+1] = suffix # sic, insert suffix after dot
     ed.dot = ed.dot + 1
     editline.point = 0
@@ -37,7 +37,7 @@ def delete_backward_char(keycode):
     """
     if editline.point > 0:
         # Calls editline.delete_backward_char, thanks to keycode
-        editline.elcmd_aref(keycode, editline.point, ed.buffer[dot])
+        editline.elcmd_aref(keycode, editline.point, ed.buffer[ed.dot])
     else: 
         join_prev() # see above
 def join_next(keycode):
@@ -49,11 +49,21 @@ def delete_char(keycode):
     If point is not at end of line, delete character under cursor.
     Otherwise join next line to this one.  At end of last line do nothing.
     """
-    if editline.point < len(ed.buffer[dot]):
+    if editline.point < len(ed.buffer[ed.dot]):
         # Calls editline.delete_char, thanks to keycode
-        editline.elcmd_aref(keycode, editline.point, ed.buffer[dot])
+        editline.elcmd_aref(keycode, editline.point, ed.buffer[ed.dot])
     else:
         join_next() # see above
+
+def kill_line(keycode):
+    """
+    Kill entire line(s) or kill the rest of line at dot
+    """
+    if True: # FIXME for now always use dmacs kill_line, work out logic later
+        dmacs.kill_line()
+    else:
+        # Calls editline kill_line, thanks to keycode
+        editline.elcmd_aref(keycode, editline.point, ed.buffer[ed.dot])
 
 def yank(keycode):
     """
@@ -63,7 +73,7 @@ def yank(keycode):
         ed.y() # yank entire line(s)
     else:
         # Calls editline.yank, thanks to keycode
-        editline.elcmd_aref(keycode, editline.point, ed.buffer[dot])
+        editline.elcmd_aref(keycode, editline.point, ed.buffer[ed.dot])
         return editline.yank(point, line) # yank word(s) within a line
 
 def refresh(keycode):
@@ -74,9 +84,10 @@ def refresh(keycode):
 
 keymap = {
     key.cr: open_line, 
-    key.C_d: delete_char,
     key.delete: delete_backward_char,
     key.bs: delete_backward_char, 
+    key.C_d: delete_char,
+    key.C_k: kill_line,
     key.C_y: yank,
     key.C_l: refresh,
 }
@@ -92,7 +103,7 @@ def pm():
     pmacs editor: invoke editor functions with emacs control keys.
     Exit by typing M_x (that's alt X), like emacs 'do command'.
     """
-    open_promptline()
+    dmacs.open_promptline()
     terminal.set_char_mode()
     while True:
         c = terminal.getchar()
@@ -102,10 +113,10 @@ def pm():
                 break
             elif k in keymap:
                 pmcmd(k)
-            elif k in editline.keymap:
+            elif k in editline.printing_chars or k in editline.keymap:
                 editline.elcmd_aref(k, ed.buffer, ed.dot)
             elif k in dmacs.keymap:
                 dmacs.dmcmd(k)
-    close_promptline()
+    dmacs.close_promptline()
     display.put_cursor(edsel.tlines, 1) # return cursor to command line
     terminal.set_line_mode()
