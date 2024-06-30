a896f65e46735045e10642fe2db61cd4f70302f7
jon-jacky <jon.p.jacky@gmail.com>
2024-06-30 14:27:32 -0700

    atimers: add destination arg

--- coroutines/atimers.py
+++ coroutines/atimers.py
@@ -6,10 +6,10 @@ This module is named atimers (with an s) but the coroutine is named atimer
 (no s) so we can 'from atimers import atimer' without name conflict. 
 """
 
-import asyncio, datetime
+import sys, asyncio, datetime
 
 # Based on timer from timers.py
-async def atimer(n=1, delay=1.0, label=''):
+async def atimer(n=1, delay=1.0, label='', destination=sys.stdout):
     """ 
     Sleep for given delay (default 1.0 sec), then print timestamp message.
     Repeat n times (default 1).  Message includes time and optional label. 
@@ -20,5 +20,7 @@ async def atimer(n=1, delay=1.0, label=''):
     for i in range(n):
         await asyncio.sleep(delay)
         # For now use print with default args, which adds the \n itself.
-        print(f'{label} {i+1} {datetime.datetime.now()}\n\r', end='')
+        print(f'{label} {i+1} {datetime.datetime.now()}\n\r', end='',
+              file=destination)
+
 
