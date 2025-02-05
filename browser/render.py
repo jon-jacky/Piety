a5fc372d3a8265c11bd0fa140c5fbad9ef41c74d
Your Name <you@example.com>
2025-02-05 15:52:47 -0800

    render: <p> paragraphs now handled as we want each paragraph filled, separate paragraphs separated by empty lines BUT <strong> elments within paragraphs appear on lines by themselves separated by empty lines before and after!

--- browser/render.py
+++ browser/render.py
@@ -22,39 +22,57 @@ class HTML2Text(HTMLParser):
         
     Example:                         
     from render import HTML2Text
-    html2text = HTML2Text()
-    html2text.feed(''.join(ed.buffer)) # requires string, not list of string
-    for line in html2text.data: print(line.rstrip()) #  remove extra \n
+    parser = HTML2Text()
+    parser.feed(''.join(ed.buffer)) # requires string, not list of string
+    for line in parser.data: print(line.rstrip()) #  remove extra \n
     """
     def __init__(self):
         super().__init__()
-        self.data = []
-        self.capture = False
-                            
+        # self.output is onw big string with embedded \n indicating lines
+        self.output = ''  # Accumulate parsed and formatted data here.
+        self.capture = False # Not in any supported tag, discard html 
+        # self.tags is needed so handle_data can handle each tag differently.
+        # self.tags is a list so we can append and pop tags for nested elements
+        # self.tags[-1] is the tag for the current, possibly deeply nested elt.
+        self.tags = [] # empty tags list means we are not in any supported tag.
+        
     def handle_starttag(self, tag, attrs):
-        if tag in ('p', 'h1', 'h2', 'h3', 'h4'):
+        # List of only the tags we handle.  We don't handle most tags.
+        if tag in ('p', 'h1', 'h2', 'h3', 'h4', 'strong', 'em'):
+            self.tags.append(tag)
             self.capture = True
                                                     
     def handle_endtag(self, tag):
-        if tag in ('p', 'h1', 'h2', 'h3', 'h4'):
-            self.capture = False
-            
+        # Again, list of only the tags we handle
+        if tag in ('p', 'h1', 'h2', 'h3', 'h4', 'strong', 'em'):
+            self.tags.pop()
+            if not self.tags: # tags list empty, not in any supported tag
+                self.capture = False
+                                        
     def handle_data(self, data):
         if self.capture: 
-            self.data.append('\n' + data) # blank lines between paragraphs
-             
+            tag = self.tags[-1]
+            if tag in ('p', 'h1', 'h2', 'h3', 'h4'):
+                self.output += '\n' # Separate this element with empty line.
+                # data can be long string. fill() can insert \n to break lines
+                self.output += textwrap.fill(data) + '\n' # add final linebreak
+            elif tag in ('strong', 'em'):
+                self.output += f' *{data}* ' # these data go in same line
+            else:
+                pass  # more tags to come
+                
 def r():
     """
     Render current buffer, a downloaded HTML web page, to a new text buffer.
-    Current buffer is named basename or basename.html
-    New text buffer is named basename.txt
-    
+    When current buffer is named basename or basename.html,
+    then new text buffer is named basename.txt     
     Name 'r' for 'render'.  We also have sked.r, reverse search
     """ 
-    html2text = HTML2Text()
-    html2text.feed(''.join(ed.buffer)) # requires string, not list of string
+    global parser # make this global so we can inspect parser.output in REPL
+    parser = HTML2Text()
+    parser.feed(''.join(ed.buffer)) # requires string, not list of string
 
-    path = Path(ed.bufname) # extract ppath, a Path object, from Parse object
+    path = Path(ed.bufname) # create Path object from ed.bufname string
     bufname = path.stem + '.txt'  # path.stem is basename index.html -> index
     
     # From here on, the code is similar to get.py fcn g()
@@ -62,13 +80,11 @@ def r():
     ed.filename = bufname # replace filename created by e() with url
     ed.buffers[bufname]['filename'] = bufname # replace filename created by e()
     ed.buffer = ['\n'] # So content starts at index 1 not 0, like other buffers.
-    # Fill in buffer text
-    for element in html2text.data:  # each element is a <p> or <h1> or ...
-        ed.buffer.append('\n') # separate elments with empty lines
-        # element is a long string. fill() inserts \n's to define lines ...
-        filled = textwrap.fill(element).rsplit('\n') # ... rsplit() makes list.
-        for line in filled:
-            ed.buffer.append(line + '\n') # each line in buffer ends with \n
+    for line in parser.output.rsplit('\n'): # make list of lines from string
+        ed.buffer.append(line + '\n') # each line in buffer ends with \n    
     fr.refresh()
     ed.dot = 1 # first line of content, top line of window, is index 1 not 0
     print(f'{ed.bufname}, {len(ed.buffer)} lines') # after '0 lines' from e()
+  
+
+    
