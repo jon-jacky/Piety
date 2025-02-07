db4fa68337898d7be194f4c14285d2c1aadaddb8
Your Name <you@example.com>
2025-02-07 13:20:18 -0800

    render: links from <a href...> tags are shown as footnotes, with link number as footnotes inline in buffer and numbered urls at the bottom

--- browser/render.py
+++ browser/render.py
@@ -8,7 +8,7 @@ import textwrap
 
 import sked as ed
 import edsel as fr # fr for frame
-
+ 
 class HTML2Text(HTMLParser):
     """
     Render HTML to text, using Python standard library HTMLParser.
@@ -38,11 +38,19 @@ class HTML2Text(HTMLParser):
         # handle_data accumulates data in self.paragraph,
         # handle_endtag formats self.paragraph
         self.paragraph = ''
-        
+        # Each link is numbered in place in the buffer like a footnote.
+        # Then all the numbered link URLs are printed at the bottom of the buf.
+        self.linknum = 0 # footnote number, first link gets linknum 1
+        self.links = [] # indexed 0 .. final linknum - 1
+        self.linkurl = '' # assigned by handle_starttag
+        self.placeholder = 'https://nowhere.com/unknown.html'
+                 
     def handle_starttag(self, tag, attrs):
         # List of only the tags we handle.  We don't handle most tags.
         if tag in ('p', 'h1', 'h2', 'h3', 'h4'):
             self.paragraph = '' # start a new paragraph
+        if tag == 'a':  # <a href=linkurl>...</a> 
+            self.linkurl = attrs[0][1] # assumes attrs[0] is ('href', url)
         if tag in ('p', 'h1', 'h2', 'h3', 'h4', 'strong', 'em', 'a' ):
             self.tags.append(tag)  # push tag onto stack of tags
             self.capture = True
@@ -66,7 +74,9 @@ class HTML2Text(HTMLParser):
             elif tag in ('strong', 'em'):
                 self.paragraph += f' *{data}* ' # these data go in same para.
             elif tag == 'a': # hypertext link, usually href=...
-                self.paragraph += f'_{data.strip()}_ [link]' # in same para.
+                self.linknum += 1 # footnotes count up from 1 not 0
+                self.links.append(self.linkurl) # linkurl from handle_starttag
+                self.paragraph += f'_{data.strip()}_ [{self.linknum}]'
             else:
                 pass  # more tags to come
                 
@@ -91,6 +101,9 @@ def r():
     ed.buffer = ['\n'] # So content starts at index 1 not 0, like other buffers.
     for line in parser.output.rsplit('\n'): # make list of lines from string
         ed.buffer.append(line + '\n') # each line in buffer must end with \n    
+    ed.buffer.append('\n\n')
+    for ilink in range(parser.linknum):
+        ed.buffer.append(f'{ilink+1}: {parser.links[ilink]}\n')         
     fr.refresh()
     ed.dot = 1 # first line of content, top line of window, is index 1 not 0
     print(f'{ed.bufname}, {len(ed.buffer)} lines') # after '0 lines' from e()
