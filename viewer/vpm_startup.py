feedce5fbfafa8982c641f3805489ef70d7ded6b
jon-jacky <jon.p.jacky@gmail.com>
2026-02-10 21:00:55 -0800

    much more text in viewer/desktop.txt, also keys.txt vpm_startup.py

--- viewer/vpm_startup.py
+++ viewer/vpm_startup.py
@@ -1,6 +1,9 @@
 # vpm_startup.ph invoked from vpm.py   Assumes Piety is current directory
 import pmacs, edsel as fr
-fr.e('viewer/desktop.txt') # assumes we are in Piety directory 
+fr.e('viewer/keys.txt')
+fr.e('viewer/viewer.py')
+fr.e('viewer/README.md')
+fr.e('viewer/desktop.txt') # load this one last, so it appears in viewer
 pmacs.pm()    # start display editing in viewer window
 ###N()     # list buffers in viewer window
 ###oe()   # switch focus back to editor window
